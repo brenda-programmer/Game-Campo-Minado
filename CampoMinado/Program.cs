98bb03e270c877eaba905a9062658a40c181185c
Brenda Oliveira <brenda.programmer@gmail.com>
2019-08-28 10:18:44 -0300

    Projeto iniciado mostrando campo minado inicial

--- CampoMinado/Program.cs
+++ CampoMinado/Program.cs
@@ -4,9 +4,51 @@ namespace CampoMinado
 {
     class Program
     {
-        static void Main(string[] args)
+        static string[,] mina = new string[5, 5];
+        static string[,] campo = new string[5, 5];
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            //----------POPULA MATRIZ CAMPO------------------------------------
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    campo[i, j] = "#";
+                }
+            }
+
+            // Mostra o campo inicial
+            CampoInicial();
+
+        }
+
+        //-----------FUNÇÃO QUE MOSTRA CAMPO INICIAL------------------------
+        static void CampoInicial()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("▒▒▒▒▒ CAMPO MINADO ▒▒▒▒▒ ");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("     1  2  3  4  5");
+            Console.WriteLine("   ╔═══════════════╗ ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(" " + (i + 1) + " ║");
+
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(" " + campo[i, j] + " ");
+                }
+                Console.Write("║");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("   ╚═══════════════╝ ");
+            Console.WriteLine("               0 pts");
+
+
         }
     }
 }
